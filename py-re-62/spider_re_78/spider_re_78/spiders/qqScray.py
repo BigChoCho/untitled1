a8ec41c50d8c0d2e566be02b7744096ff8323911
BigChoCho <a3858117@gmail.com>
2019-05-15 22:02:11 +0800

    2

--- py-re-62/spider_re_78/spider_re_78/spiders/qqScray.py
+++ py-re-62/spider_re_78/spider_re_78/spiders/qqScray.py
@@ -15,7 +15,6 @@ class qqscray(scrapy.Spider):
         'https://careers.tencent.com/tencentcareer/api/post/Query?timestamp=1557895020042&countryId=1&cityId=&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=&pageIndex=1&pageSize=10&language=zh-cn&area=us'
     ]
     def parse(self, response):
-        print("qqscray")
         print("為何沒咚咚")
         #decode=解碼encode=編碼,json.loads轉成字典類型
         dicts=json.loads(response.body.decode().encode('utf-8'))
@@ -27,30 +26,49 @@ class qqscray(scrapy.Spider):
 
         print("_________________"*5)
         #藉由無頭瀏覽器獲取動態js
-        driver = webdriver.Chrome(r'C:\Users\a3858\PycharmProjects\untitled1\chromedriver.exe')
-        driver.get('https://careers.tencent.com/search.html?query=co_1&sc=1')
-        time.sleep(10)
-        #等待3秒
-        driver.implicitly_wait(3)
-        tree = etree.HTML(driver.page_source)
-        etree.tostring(tree)
-
-        tress = tree.xpath('//div[@class="recruit-wrap recruit-margin"]/div')
-        for names,href in zip(tress,dicts):
+        # driver = webdriver.Chrome(r'C:\Users\a3858\PycharmProjects\untitled1\chromedriver.exe')
+        # driver.get('https://careers.tencent.com/search.html?query=co_1&sc=1')
+        # time.sleep(10)
+        # #等待3秒
+        # driver.implicitly_wait(3)
+        # tree = etree.HTML(driver.page_source)
+        # etree.tostring(tree)
+        #
+        # tress = tree.xpath('//div[@class="recruit-wrap recruit-margin"]/div')
+        for href in dicts:
             item = QQItem()
             detailLink="https://careers.tencent.com/jobdesc.html?postId="+href['PostId']
-            name=names.xpath('./a/h4[@class="recruit-title"]/text()')
-            positionInfo=names.xpath('./a/p[@class="recruit-text"]/text()')
-            workLocations = names.xpath('./a/p[@class="recruit-tips"]/span/text()')
-            workLocation=workLocations[0],",地點:"+workLocations[1],",類型:"+workLocations[2],",發布日期:"+workLocations[3]
+            name = href["RecruitPostName"]
+            positionInfo=href["Responsibility"]
+            workLocation ="地點:"+href["CountryName"]+",類型:"+href['LocationName']+",發布日期:"+href["LastUpdateTime"]
+            # name=names.xpath('./a/h4[@class="recruit-title"]/text()')
+            # positionInfo=names.xpath('./a/p[@class="recruit-text"]/text()')
+            # workLocations = names.xpath('./a/p[@class="recruit-tips"]/span/text()')
+            # workLocation=workLocations[0],",地點:"+workLocations[1],",類型:"+workLocations[2],",發布日期:"+workLocations[3]
 
 
-            item['name'] = name[0]
+            # item['name'] = name[0]
+            item['name']=name
             item["detailLink"]=detailLink
-            item['positionInfo']=positionInfo[0]
+            item['positionInfo']=positionInfo
             item['workLocation']=workLocation
 
+            # 处理继续爬去的链接
+            # 通过得到当前页，提取数字，把数字加1，替换原来的数字，就是下一个页面地址
+            # 提取当前也的数字
 
+            curpage = re.findall('(\d+)', response.url)
+            curpage = curpage[2]
+            # 生成下一页url
+            page = int(curpage) + 1
+            url = response.url.replace("Index=1", "Index=" + str(page))
+            print(url)
+
+            time.sleep(10)
+            # 把地址通过yield返回
+            # 注意callback的写法
+            #處理完再調用parse函數,並且把url回傳response
+            yield scrapy.Request(url,callback=self.parse)
 
             yield item
 
--- py-re-62/test.py
+++ py-re-62/test.py
@@ -1,29 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
-import json
-import pymongo
-
-url = 'http://www.guokr.com/scientific/'
+import re
 
 
-def dealData(url):
-    client = pymongo.MongoClient('localhost', 27017)
-    guoke = client['guoke']
-    guokeData = guoke['guokeData']
-    web_data = requests.get(url)
-    datas = json.loads(web_data.text)
-    print
-    datas.keys()
-    for data in datas['result']:
-        guokeData.insert_one(data)
-
-
-def start():
-    urls = [
-        'http://www.guokr.com/apis/minisite/article.json?retrieve_type=by_subject&limit=20&offset={}&_=1508843461033'.format(
-            str(i)) for i in range(20, 100, 20)]
-    for url in urls:
-        dealData(url)
-
-
-start()
+url ='https://careers.tencent.com/tencentcareer/api/post/Query?timestamp=1557895020042&countryId=1&cityId=&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=&pageIndex=1&pageSize=10&language=zh-cn&area=us'
+curpage = re.findall('(\d+)', url)
+curpage=curpage[2]
+page = int(curpage) + 1
+url = url.replace("Index=1","Index="+str(page))
+print(url)
